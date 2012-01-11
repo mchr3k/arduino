28415b2ade17b1def9a3b71c930bff825435e372
Martin <mchr3k@gmail.com>
2012-01-11 00:03:54 +0000

    Add time lapse code

---
+++ timelapseservo/timelapseservo.ino
@@ -0,0 +1,34 @@
+// Sweep
+// by BARRAGAN <http://barraganstudio.com> 
+// This example code is in the public domain.
+
+
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+                // a maximum of eight servo objects can be created 
+ 
+int pos = 0;    // variable to store the servo position 
+ 
+void setup() 
+{ 
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+} 
+
+#define ROT_BASE 36
+#define ROT_END 60
+ 
+void loop() 
+{ 
+  for(pos = ROT_BASE; pos < ROT_END; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+  delay(1000);
+  for(pos = ROT_END; pos>=ROT_BASE; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+} 
